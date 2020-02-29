d2e0b32caffcfc98af6ae2c8b329ec782d53bae0
nick <hawhaw.ya@gmail.com>
2020-02-29 14:58:41 +0800

    add postfix [ 'testFunc', 'benchmarkFunc' ]

---
+++ src/templates/testFunc.ts
@@ -0,0 +1,40 @@
+"use strict";
+
+import * as vscode from 'vscode';
+
+const { CompletionItemBuilder } = require("../completionItemBuilder");
+const { BaseExpressionTemplate } = require("./baseFunc");
+const utils = require("../utils");
+
+export class TestFunc extends BaseExpressionTemplate {
+
+    applyItem(code: string, position: vscode.Position) {
+        let lastComponent = utils.getLastComponent(code, position);
+        let upCase = utils.capitalization(lastComponent);
+
+        return CompletionItemBuilder
+            .create('testFunc', lastComponent)
+            .description('quickly create test func')
+            .replace('func Test' + upCase + `(t *testing.T) {\n ${utils.getIndentCharacters()}\${0}\n}`, position, true)
+            .deleteTextBeforeCursor(position, lastComponent.length + 1).build();
+    }
+}
+
+export class BenchmarkFunc extends BaseExpressionTemplate {
+    
+    applyItem(code: string, position: vscode.Position) {
+        let lastComponent = utils.getLastComponent(code, position);
+        let upCase = utils.capitalization(lastComponent);
+
+        return CompletionItemBuilder
+            .create('benchmarkFunc', lastComponent)
+            .description('create Benchmark func')
+            .replace('func Benchmark' + upCase + `(b *testing.B) {\n ${utils.getIndentCharacters()}\${0}\n}`, position, true)
+            .deleteTextBeforeCursor(position, lastComponent.length + 1).build();
+    }
+}
+
+exports.build = () => [
+    new TestFunc(),
+    new BenchmarkFunc()
+];
